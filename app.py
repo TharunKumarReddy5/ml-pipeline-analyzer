c85696bb32d22116273221e1a9d0f5c183010dae
Sravan Hande <sravankr96@gmail.com>
2022-02-20 23:56:00 -0800

    Adds pkl file upload feature

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, url_for, redirect
+import os
+import time
+from mlpipeline_analyzer.visualizer import PipelineDiagram
+import joblib
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def get_app():
-    return render_template('index.html')
+    return render_template('index.html', view_id='')
 
 
 @app.route('/', methods=['POST'])
 def upload_file():
     uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        uploaded_file.save(uploaded_file.filename)
-    return redirect(url_for('index'))
+    if uploaded_file.filename != '' and uploaded_file.filename.endswith('pkl'):
+        fp = os.path.join('./uploads', 'model' + str(time.time()) + '.pkl')
+        uploaded_file.save(fp)
+        PipelineDiagram(joblib.load(fp))
+    return render_template('index.html', view_id='tryit')
 
 
 if __name__ == '__main__':

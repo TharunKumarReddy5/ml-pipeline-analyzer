6e6e336f2172643fdeb8034ea324362841dcade1
Sravan Hande <sravankr96@gmail.com>
2022-02-23 23:44:20 -0800

    Add changes to deploy and reorganize code

--- app/main.py
+++ app/main.py
@@ -29,16 +29,15 @@ def upload_file():
         fname = str(time.time()) + '.pkl'
         fp = os.path.join(os.getcwd(), 'uploads', 'model' + fname)
         uploaded_file.save(fp)
-        print("plk file:" + fp)
         PipelineDiagram(joblib.load(fp), file_name=fname[:-4]).create_diagram()
         pipeline_img_nm = fname[:-3] + 'png'
-        pipeline_img_dest = os.path.join(os.getcwd(), 'static', pipeline_img_nm)
+        pipeline_img_dest = os.path.join(os.getcwd(), 'app', 'static', pipeline_img_nm)
         os.rename(pipeline_img_nm, pipeline_img_dest)
-        print("file saved:" + fname[:-3] + 'png')
         executor.submit(file_clean_up, [fp, pipeline_img_dest])
         return render_template('index.html', view_id='tryit', pipeline_img='/static/' + fname[:-3] + 'png')
-    except Exception:
+    except Exception as e:
         print("Exception occurred with file: " + fp)
+        print(e)
         return render_template('index.html', view_id='tryit', pipeline_img='', alt_text=PIPELINE_ERROR_MSG)
 
 

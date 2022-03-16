c3ba3cccde2f1d0038431efb029511beedbbe0a5
Tharun Kumar Reddy Karasani <karasani.tarunreddy@gmail.com>
2022-03-16 00:28:08 -0700

    Update test_pipelineDiagram.py

--- tests/test_mlpipeline_analyzer/test_visualizer/test_pipelineDiagram.py
+++ tests/test_mlpipeline_analyzer/test_visualizer/test_pipelineDiagram.py
@@ -3,30 +3,24 @@ import graphviz
 import joblib
 from mlpipeline_analyzer.visualizer.PipelineDiagram import PipelineDiagram
 
-
-class TestPipelineDiagram:
-         
+class TestPipelineDiagram:   
     def test_show_sklearn():
         sklearn_pipe = joblib.load('examples/sample_models/ml_pipeline.pkl')
         diagram = PipelineDiagram(sklearn_pipe)
         with diagrams.Diagram('', show=False, filename=''):
             assert diagrams.Diagram in diagram.show(title='Sklearn ML Pipeline Diagram').__class__.mro()
-            
     def test_show_params_sklearn():
         sklearn_pipe = joblib.load('examples/sample_models/ml_pipeline.pkl')
         diagram = PipelineDiagram(sklearn_pipe)
         with diagrams.Diagram('', show=False, filename=''):
-            assert graphviz.dot.Digraph in diagram.show_params(title='Sklearn Machine Learning Parameters Pipeline').__class__.mro()
-    
+            assert graphviz.dot.Digraph in diagram.show_params(title='Sklearn Machine Learning Parameters Pipeline').__class__.mro()   
     def test_show_evalml():
         evalml_pipe = joblib.load('examples/sample_models/automl_pipeline.pkl')
         diagram = PipelineDiagram(evalml_pipe)
         with diagrams.Diagram('', show=False, filename=''):
-            assert diagrams.Diagram in diagram.show(title='Sklearn ML Pipeline Diagram').__class__.mro()
-            
+            assert diagrams.Diagram in diagram.show(title='Sklearn ML Pipeline Diagram').__class__.mro()            
     def test_show_params_evalml():
         evalml_pipe = joblib.load('examples/sample_models/automl_pipeline.pkl')
         diagram = PipelineDiagram(evalml_pipe)
         with diagrams.Diagram('', show=False, filename=''):
             assert graphviz.dot.Digraph in diagram.show_params(title='Sklearn Machine Learning Parameters Pipeline').__class__.mro()
-

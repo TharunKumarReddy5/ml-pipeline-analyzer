c778640d539e9413a058001b0578f8631facaf24
Tharun Kumar Reddy Karasani <karasani.tarunreddy@gmail.com>
2022-03-17 14:37:30 -0700

    Added the documents folder

--- setup.py
+++ setup.py
@@ -8,17 +8,18 @@ classifiers = [
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
     'Operating System :: Microsoft :: Windows :: Windows 10',
-    'Topic :: Scientific/Engineering :: Machine Learning',
-    'Topic :: Scientific/Engineering :: Visualization',
+    #'Topic :: Scientific/Engineering :: Machine Learning',
+    #'Topic :: Scientific/Engineering :: Visualization',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3'
+    'Programming Language :: Python :: 3.8'
 ]
 
 setup(
     name='mlpipeline_analyzer',
-    version='0.0.1',
+    version='0.0.2',
     description='Python package that analyze, visualize and suggest any changes to the machine learning pipeline ',
-    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
     author='Tharun Kumar Reddy Karasani',
     author_email='karasani.tarunreddy@gmail.com',
     url='https://github.com/TharunKumarReddy5/ml-pipeline-analyzer',
